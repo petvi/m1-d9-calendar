b4530cf417d69a435007cdc7ac5899439e8707b4
petvi <petrasvilkelis@gmail.com>
2020-10-08 12:28:27 +0200

    Add Day Selection

--- main.js
+++ main.js
@@ -8,6 +8,21 @@ function daysInMonth() {
   ).getDate();
 }
 
+function dayClick(e) {
+  const $day = e.target;
+
+  const old = document.querySelector(".selected");
+
+  if (old) old.classList.remove("selected");
+
+  $day.classList.add("selected");
+
+  console.log(e.target.innerText);
+
+  document.querySelector("#selection").innerText =
+    e.target.innerText;
+}
+
 const $heading = document.querySelector("h1");
 
 const $calendar = document.querySelector(
@@ -25,5 +40,6 @@ for (let i = 0; i < numOfDays; i++) {
   const $day = document.createElement("div");
   $day.classList.add("day");
   $day.innerText = i + 1;
+  $day.addEventListener("click", dayClick);
   $calendar.appendChild($day);
 }
